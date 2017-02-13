afcfc99a12485c9ff7f2dcf0508c968cdd4ec495
Marcelo Busico <marcelo@busico.com.ar>
2017-02-13 02:21:39 -0300

    Implementing formatter for time since submission.

--- Reddit-App/Reddit-App/ListReddits/Model/RedditModel.swift
+++ Reddit-App/Reddit-App/ListReddits/Model/RedditModel.swift
@@ -34,4 +34,38 @@ struct RedditModel {
     self.thumbnailURL = thumbnailURL
     self.imageURL = imageURL
   }
+
+  func formattedTimeSinceSubmissions() -> String {
+    let currentDate = Date()
+    let timeInterval = currentDate.timeIntervalSince(dateCreated)
+
+    if timeInterval < 60 {
+      return "just now"
+    }
+
+    let minutes = Int(timeInterval / 60)
+    if minutes < 60 {
+      if minutes == 1 {
+        return "a minute ago"
+      } else {
+        return "\(minutes) minutes ago"
+      }
+    }
+
+    let hours = Int(minutes / 60)
+    if hours < 24 {
+      if hours == 1 {
+        return "an hour ago"
+      } else {
+        return "\(hours) hours ago"
+      }
+    }
+    
+    let days = Int(hours / 24)
+    if days == 1 {
+      return "a day ago"
+    } else {
+      return "\(days) days ago"
+    }
+  }
 }
